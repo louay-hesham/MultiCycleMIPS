aa195b0591e72f76400e1cc1d164020bce056df9
louay-hesham <louay.hesham@gmail.com>
2017-04-25 13:06:03 +0200

    restored memory files

--- dmem.sv
+++ dmem.sv
@@ -1,9 +1,7 @@
-module memory(	input logic clk, we,
+module dmem(	input logic clk, we,
 		input logic [31:0] a, wd, 
 		output logic [31:0] rd); 
 	logic [31:0] RAM[63:0]; 
-	initial
-		$readmemh("memfile.dat", RAM);
 	assign rd=RAM[a[31:2]]; // word aligned
 	always_ff @(posedge clk) 
 		if (we) 
