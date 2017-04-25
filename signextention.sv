91492b9897cbb258f80cf3cc1878778e48b5b77c
louay-hesham <louay.hesham@gmail.com>
2017-04-25 13:05:34 +0200

    restored sign extension to its original code

--- signextention.sv
+++ signextention.sv
@@ -1,6 +1,5 @@
 module signext(	input logic [15:0] a,
-		input logic signOrZero,
 		output logic [31:0] y);
-	assign y = signOrZero? {{16'b0},a} : {{16{a[15]}},a}; 
+	assign y={{16{a[15]}}, a};
 endmodule
 
