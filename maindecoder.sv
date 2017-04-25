eccec58bedd151bd3e0bba9fe7efdf92b9b3f3a8
louay-hesham <louay.hesham@gmail.com>
2017-04-25 20:09:58 +0200

    state 1 in control unit state machine

--- maindecoder.sv
+++ maindecoder.sv
@@ -5,14 +5,14 @@ module maindec(	input logic clk,
 		output logic alusrcA,
 		output logic [1:0] alusrcB, aluop, pcsrc);
 
-	logic [2:0] state;
-	initial	state = 3b'000;
+	logic [3:0] state;
+	initial	state = 4'b0000;
 
-	always_comb
+	always @(posedge clk)
 		case(state)
-			3'b000:
+			4'b0000: //fetch
 			begin
-				state = 3'b001;
+				state = 4'b0001;
 				assign IorD = 0;
 				assign alusrcA = 0;
 				assign alusrcB = 2'b01;
@@ -21,12 +21,21 @@ module maindec(	input logic clk,
 				assign IRwrite = 1;
 				assign pcwrite = 1;
 			end
-			3'b001:
+			4'b0001: //decode
 			begin
-
+				assign alusrcA = 0;
+				assign alusrcB = 2'b11;
+				assign aluop = 2'b00;
+				case (op)
+					6'b000000: state = 4'b0110; // RTYPE
+					6'b100011: state = 4'b0010; // LW
+					6'b101011: state = 4'b0010; // SW
+					6'b000100: state = 4'b1000; // BEQ
+					6'b001000: state = 4'b1001; // ADDI
+					6'b000010: state = 4'b1011; // J
+					default:   state = 4'bxxxx; // illegal op
+				endcase
 			end;
-			3'b010:
-			3'b011:
-			3'b100:
+			
 		endcase
 endmodule
