91230d3a211a850cd551d7cbf687945a1591157a
louay-hesham <louay.hesham@gmail.com>
2017-04-26 21:49:43 +0200

    added datapath module to mips and modified top

--- top.sv
+++ top.sv
@@ -3,7 +3,7 @@ module top(	input logic clk, reset,
 		output logic memwrite);
 	logic [31:0] pc, instr, readdata; 	
 	// instantiate processor and memories
-	mips mips(clk, reset, pc, instr, memwrite, dataadr, writedata, readdata);
+	mips mips(clk, reset, pc, instr, memwrite, dataadr, writedata, dataadr, readdata);
 	mem memory(clk, memwrite, dataadr, writedata, readdata);
 endmodule
 
