c9ee230ec282ce2564c02b9bf9e80de0ef339752
louay-hesham <louay.hesham@gmail.com>
2017-04-26 20:13:23 +0200

    unifying instruction and data memories

--- dmem.sv
+++ dmem.sv
@@ -1,6 +1,9 @@
-module dmem(	input logic clk, we,
+module mem(	input logic clk, we,
 		input logic [31:0] a, wd, 
 		output logic [31:0] rd); 
+	initial
+		$readmemh("memfile.dat", RAM);
+
 	logic [31:0] RAM[63:0]; 
 	assign rd=RAM[a[31:2]]; // word aligned
 	always_ff @(posedge clk) 
